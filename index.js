00eae2108116a8bb45160aee9b8bccec1053089f
Farbod Farhang <37166633+farbodf1@users.noreply.github.com>
2018-10-30 20:04:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -26,8 +26,12 @@ app.get('/', (request, response) => {
 
 app.get('/greeting', (request, response) => {
   // FIXME: If a name is not given, the app says 'Hello undefined'
+  try{
   const message = `Hello ${request.query.name}`;
   response.status(200).send(message);
+  } catch (e){
+    console.log(e);
+  }
 });
 
 app.get('/todo', async (request, response) => {
